c8d7999986a96af17bbd1c558ed515590b03db11
RAJESH2132 <rajeshnagurla@gmail.com>
2025-09-22 21:49:08 +0530

    refactored server js code

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,6 +9,16 @@ app.get("/", (req, res) => {
   res.send("Server running");
 });
 
-app.listen(ENV.PORT, () => {
-  console.log("server up and running on:", ENV.PORT);
-});
+const startServer = async () => {
+  try {
+    await connectDB();
+
+    app.listen(ENV.PORT, () => {
+      console.log("server up and running on:", ENV.PORT);
+    });
+    app;
+  } catch (error) {
+    console.log("Failed to start server:", error.message);
+    process.exit(1);
+  }
+};
