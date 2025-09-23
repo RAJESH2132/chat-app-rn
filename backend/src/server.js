51f803d811e036df0ce659b7f79ba8c0b6b377b2
RAJESH2132 <rajeshnagurla@gmail.com>
2025-09-23 22:10:12 +0530

    Add user management features and authentication middleware
    - Implement user profile retrieval, update, and synchronization with Clerk.
    - Add follow/unfollow functionality with notifications.
    - Introduce authentication middleware to protect routes.
    - Update server configuration to use environment variables.
    - Add necessary dependencies: cloudinary, express-async-handler, multer.

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,24 @@
 import express from "express";
+import { ENV } from "./config/env.js";
+import { connectDB } from "./config/db.js";
 
 const app = express();
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("Server running");
 });
 
-app.listen(5001, () => {
-  console.log("server up and running");
-});
+const startServer = async () => {
+  try {
+    await connectDB();
+
+    app.listen(ENV.PORT, () => {
+      console.log("server up and running on:", ENV.PORT);
+    });
+    app;
+  } catch (error) {
+    console.log("Failed to start server:", error.message);
+    process.exit(1);
+  }
+};
