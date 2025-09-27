c9c3d4bed5785c57bd3022dafe66fe0542e830b1
RAJESH2132 <rajeshnagurla@gmail.com>
2025-09-27 12:13:24 +0530

    auth implemented

---
+++ mobile/app/(auth)/index.tsx
@@ -0,0 +1,96 @@
+// import { useSocialAuth } from "@/hooks/useSocialAuth";
+import { useSocialAuth } from "@/hooks/useSocialAuth";
+import {
+  ActivityIndicator,
+  Image,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+
+export default function Index() {
+  const { handleSocialAuth, isLoading } = useSocialAuth();
+
+  return (
+    <View className="flex-1 bg-white">
+      <View className="justify-between flex-1 px-8">
+        <View className="justify-center flex-1">
+          <View className="items-center">
+            <Image
+              source={require("../../assets/images/auth2.png")}
+              className="size-96"
+              resizeMode="contain"
+            />
+          </View>
+
+          <View className="flex-col gap-2">
+            <TouchableOpacity
+              className="flex-row items-center justify-center px-6 py-3 bg-white border border-gray-300 rounded-full"
+              onPress={() => handleSocialAuth("oauth_google")}
+              disabled={isLoading}
+              style={{
+                shadowColor: "#000",
+                shadowOffset: { width: 0, height: 1 },
+                shadowOpacity: 0.1,
+                shadowRadius: 2,
+                elevation: 2,
+              }}
+            >
+              {isLoading ? (
+                <ActivityIndicator size="small" color="#4285F4" />
+              ) : (
+                <View className="flex-row items-center justify-center">
+                  <Image
+                    source={require("../../assets/images/google.png")}
+                    className="mr-3 size-10"
+                    resizeMode="contain"
+                  />
+                  <Text className="text-base font-medium text-black">
+                    Continue with Google
+                  </Text>
+                </View>
+              )}
+            </TouchableOpacity>
+
+            <TouchableOpacity
+              className="flex-row items-center justify-center px-6 py-3 bg-white border border-gray-300 rounded-full"
+              onPress={() => handleSocialAuth("oauth_apple")}
+              disabled={isLoading}
+              style={{
+                shadowColor: "#000",
+                shadowOffset: { width: 0, height: 1 },
+                shadowOpacity: 0.1,
+                shadowRadius: 2,
+                elevation: 2,
+              }}
+            >
+              {isLoading ? (
+                <ActivityIndicator size="small" color="#000" />
+              ) : (
+                <View className="flex-row items-center justify-center">
+                  <Image
+                    source={require("../../assets/images/apple.png")}
+                    className="mr-3 size-8"
+                    resizeMode="contain"
+                  />
+                  <Text className="text-base font-medium text-black">
+                    Continue with Apple
+                  </Text>
+                </View>
+              )}
+            </TouchableOpacity>
+          </View>
+
+          <Text className="px-2 mt-6 text-xs leading-4 text-center text-gray-500">
+            By signing up, you agree to our{" "}
+            <Text className="text-blue-500">Terms</Text>
+            {", "}
+            <Text className="text-blue-500">Privacy Policy</Text>
+            {", and "}
+            <Text className="text-blue-500">Cookie Use</Text>.
+          </Text>
+        </View>
+      </View>
+    </View>
+  );
+}
--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,6 +1,14 @@
+import { ClerkProvider } from "@clerk/clerk-expo";
+import { tokenCache } from "@clerk/clerk-expo/token-cache";
 import { Stack } from "expo-router";
-import "../global.css"
+import "../global.css";
 
-export default function RootLayout() {
-  return <Stack />;
+export default function RootLayoutNav() {
+  return (
+    <ClerkProvider tokenCache={tokenCache}>
+      <Stack>
+        <Stack.Screen name="(auth)" options={{headerShown: false}}/>
+      </Stack>
+    </ClerkProvider>
+  );
 }
