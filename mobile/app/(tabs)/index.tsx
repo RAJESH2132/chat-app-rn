fc2fb57a8aad862723e7f7aa5f4c4601ce892642
RAJESH2132 <rajeshnagurla@gmail.com>
2025-09-27 23:18:32 +0530

    user synced to db

--- mobile/app/(tabs)/index.tsx
+++ mobile/app/(tabs)/index.tsx
@@ -2,8 +2,10 @@ import { Text } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import SignOutButton from "@/components/SignOut";
+import { useUserSync } from "@/hooks/useUserSync";
 
 const HomeScreen = () => {
+  useUserSync();
   return (
     <SafeAreaView className="flex-row justify-between flex-1 p-10">
       <Text>HomeScreen</Text>
